b3979e3e484b123c09b06063e2f1a064d7393130
Ricardo <ricardoczj@qq.com>
2020-09-14 16:35:25 +0800

    优化界面

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
 module.exports = {
 //   lintOnSave: false,
   publicPath: './',
@@ -10,5 +14,12 @@ module.exports = {
   },
   transpileDependencies: [
     'quasar'
-  ]
+  ],
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("imgs", resolve("src/imgs"))
+  },
 }
