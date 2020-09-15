4f899ca20d9a31fd85b36a8623cd5014a641883a
Ricardo <RicardoAl.com>
2020-09-15 11:23:26 +0800

    修改 api key

--- src/main.js
+++ src/main.js
@@ -10,8 +10,7 @@ import VueAMap from 'vue-amap';
 Vue.config.productionTip = false
 
 VueAMap.initAMapApiLoader({
-    key: 'ee1c92b84f6183e95149bff5139072b3',
-    // key: '1b4d55465f98f3302bf7ac47d8edc721',
+    key: '1b4d55465f98f3302bf7ac47d8edc721',
     plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor', 'AMap.Geocoder', "AMap.Geolocation","AMap.TruckDriving"],
     // v: '1.4.15',
     v: '1.4.15',
